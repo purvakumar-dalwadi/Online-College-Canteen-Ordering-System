7eed5ac72dbe956b1f4a120fa8f141a765e77cbc
purvakumar dalwadi <purvdalwadi024@gmail.com>
2026-02-24 10:51:58 +0530

    Change DBConnection password to empty string
    Updated the database connection password to an empty string for security reasons.

--- src/java/com/canteen/util/DBConnection.java
+++ src/java/com/canteen/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/canteen_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "purvd2496"; // Change this to your MySQL password
+    private static final String PASSWORD = ""; // Change this to your MySQL password
     
     static {
         try {
